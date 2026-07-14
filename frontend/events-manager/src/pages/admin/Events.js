8f430b1a891a91b4c030766a30a44c62c452e01e
NarendraReddyMeruva <narendrareddy0312@gmail.com>
2026-07-14 05:40:39 +0530

    new

--- frontend/events-manager/src/pages/admin/Events.js
+++ frontend/events-manager/src/pages/admin/Events.js
@@ -61,6 +61,7 @@ const EventsPage = () => {
 
   useEffect(() => { 
     fetchEvents(); 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleImageUpload = (e) => {
